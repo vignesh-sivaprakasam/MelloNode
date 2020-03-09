cadb098877e0f0d7001ac04df35a5fc6cc7bdbc6
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-03-09 20:57:47 +0530

    Mongo DB Setup added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express    = require("express");
+const mongoose   = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//BodyParser middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+
+//connect to Mongo
+mongoose.connect(db)
+        .then(()=> console.log("MongoDB Connected... "))
+        .catch(err =>  console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
