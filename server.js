730a666ee4193f7a6e468d291f1ecd5c65aba538
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-04-04 19:06:48 +0530

    Allowed Cross origin request

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const cards     = require('./server/Card/CardController');
 
 const app = express();
 
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+});
+
 //BodyParser middleware
 app.use(bodyParser.json());
 
