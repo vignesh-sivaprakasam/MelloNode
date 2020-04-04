c133ad04e8c6bd98e7505c4f584f873934782d8b
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-04-04 19:36:20 +0530

    Cors methods added

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
         next();
 });
 
